038112e42c8d8f4691fb6ed925aead21df80e5c4
Madin Bloch <113907236+MadinBloch@users.noreply.github.com>
2023-08-22 20:14:37 -0700

    Project

---
+++ 07_Project/colorChanger/index.js
@@ -0,0 +1,19 @@
+const button = document.querySelectorAll('.button');
+const body = document.querySelector("body")
+
+button.forEach(function (button) {
+    button.addEventListener( 'click', function (event) {
+        if(event.target.id === 'green'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'pink'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'red'){
+            body.style.backgroundColor = event.target.id;
+        }
+    })
+})
